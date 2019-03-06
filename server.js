c7593cba9525d7d806ed1fc4cf15b44bee62e77e
felsuna <felicia.sunanto@gmail.com>
2019-03-05 22:42:52 -0700

    mlab and heroku testing

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const axios = require("axios");
 const cheerio = require("cheerio");
 const db = require("./models");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -14,3 +14,12 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use(express.static("public"));
 
+// Connect to the Mongo DB.
+// If deployed, use the deployed database (mlab for Heroku). Otherwise use the local mongoHeadlines database.
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+mongoose.connect(MONGODB_URI);
+
+// Start the server.
+app.listen(PORT, function() {
+    console.log(`Server is listening on: http://localhost:${PORT}`);
+});
